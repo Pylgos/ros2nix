674f012cac270618aafa9ae348ec8c159394f457
Pylgos <43234674+Pylgos@users.noreply.github.com>
2023-01-28 14:34:46 +0900

    use source filter to prevent frequent rebuild

--- nix/common/packages.nix
+++ nix/common/packages.nix
@@ -2,14 +2,17 @@
 , cell
 }:
 let
-  inherit (inputs) self nixpkgs;
+  inherit (inputs) self nixpkgs std;
   l = nixpkgs.lib // inputs.cells.lib.lib // builtins;
 in
 rec {
   setupHelper = nixpkgs.nimPackages.buildNimPackage {
     pname = "ros2nix-setup-helper";
     version = "0.1.0";
-    src = self + "/setuphelper";
+    src = std.incl (self + /setuphelper) [
+      (self + /setuphelper/src/setuphelper.nim)
+      (self + /setuphelper/setuphelper.nimble)
+    ];
   };
 
   setupHook = nixpkgs.makeSetupHook {

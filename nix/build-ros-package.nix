407341cd6cad569ff411fb01b33f3c100f41c7d4
Pylgos <43234674+Pylgos@users.noreply.github.com>
2024-07-23 15:45:31 +0900

    separate debbug info

--- nix/build-ros-package.nix
+++ nix/build-ros-package.nix
@@ -17,6 +17,8 @@
   strictDeps ? true,
   dontWrapQtApps ? true,
   shellHook ? "",
+  extraRosCmakeArgs ? [ ],
+  separateDebugInfo ? true,
   ...
 }@args:
 let
@@ -31,8 +33,13 @@ let
     };
   } // (args.passthru or { });
 
+  rosCmakeArgs = [
+    "-DCMAKE_BUILD_TYPE=RelWithDebInfo"
+    "-DBUILD_TESTING=${if doCheck then "ON" else "OFF"}"
+  ] ++ extraRosCmakeArgs;
+
   self = stdenv.mkDerivation (
-    args
+    builtins.removeAttrs args [ "extraRosCmakeArgs" ]
     // {
       nativeBuildInputs = nativeBuildInputs ++ [
         buildPackages.python3Packages.colcon-common-extensions
@@ -50,9 +57,11 @@ let
               --install-base $out \
               --build-base /build/build \
               --event-handlers console_cohesion- console_direct+ console_package_list- console_start_end- console_stderr- desktop_notification- event_log- log- log_command- status- store_result- summary- terminal_title- \
-              --executor sequential \
-              --cmake-args -DCMAKE_BUILD_TYPE=Release \
-              --cmake-args -DBUILD_TESTING=${if doCheck then "ON" else "OFF"}
+              --executor sequential ${
+                lib.concatStringsSep " " (
+                  map (arg: "--cmake-args " + (lib.escapeShellArg (" " + arg))) rosCmakeArgs
+                )
+              }
 
             runHook postBuild
           ''
@@ -100,6 +109,7 @@ let
         strictDeps
         dontUseCmakeConfigure
         dontWrapQtApps
+        separateDebugInfo
         ;
     }
   );

11808187bc503a69707cd4a892b26ac2108e8ce7
Pylgos <43234674+Pylgos@users.noreply.github.com>
2024-08-02 17:23:46 +0900

    regenerate

--- nix/gen/jazzy/mrpt2.nix
+++ nix/gen/jazzy/mrpt2.nix
@@ -35,7 +35,7 @@
   tf2_msgs,
   tinyxml-2,
   udev,
-  wxGTK,
+  wxGTK32,
   xorg,
   zlib,
 }:
@@ -172,7 +172,7 @@ buildRosPackage {
   nativeBuildInputs = [ cmake ];
   propagatedNativeBuildInputs = [ ament_cmake pkg-config ros_environment ];
   buildInputs = [  ];
-  propagatedBuildInputs = [ assimp cv_bridge eigen ffmpeg freeglut freenect geometry_msgs glfw3 jsoncpp libGL libGLU libjpeg libpcap libusb1 nav_msgs opencv python3Packages.pip python3Packages.pybind11 rclcpp rosbag2_storage sensor_msgs std_msgs stereo_msgs suitesparse tf2 tf2_msgs tinyxml-2 udev wxGTK xorg.libXrandr zlib ];
+  propagatedBuildInputs = [ assimp cv_bridge eigen ffmpeg freeglut freenect geometry_msgs glfw3 jsoncpp libGL libGLU libjpeg libpcap libusb1 nav_msgs opencv python3Packages.pip python3Packages.pybind11 rclcpp rosbag2_storage sensor_msgs std_msgs stereo_msgs suitesparse tf2 tf2_msgs tinyxml-2 udev wxGTK32 xorg.libXrandr zlib ];
   depsTargetTarget = [  ];
   depsTargetTargetPropagated = [  ];
 }

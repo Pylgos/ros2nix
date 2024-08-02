e96bc6327e521f53b510354ca70ab0314960c9ce
Pylgos <43234674+Pylgos@users.noreply.github.com>
2024-08-02 17:07:46 +0900

    use crane

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     dream2nix.url = "github:nix-community/dream2nix";
     dream2nix.inputs.nixpkgs.follows = "nixpkgs";
     poetry2nix.url = "github:nix-community/poetry2nix";
+    crane.url = "github:ipetkov/crane";
     nix-filter.url = "github:numtide/nix-filter";
     nix-fast-build.url = "github:Mic92/nix-fast-build";
     nix-fast-build.inputs.nixpkgs.follows = "nixpkgs";
@@ -17,6 +18,7 @@
       nixpkgs,
       dream2nix,
       poetry2nix,
+      crane,
       nix-filter,
       nix-fast-build,
     }:
@@ -30,6 +32,7 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
         selfPackages = self.packages.${system};
+        craneLib = crane.mkLib pkgs;
         nix-fast-build-pkg = nix-fast-build.packages.${system}.nix-fast-build;
         filter = nix-filter.lib;
       in
@@ -42,30 +45,22 @@
           }
         ) self.overlays;
         packages = {
-          ros2nix = pkgs.callPackage (
-            {
-              rustPlatform,
-              pkg-config,
-              openssl,
-            }:
-            rustPlatform.buildRustPackage {
-              pname = "ros2nix";
-              version = "0.1.0";
-              src = filter {
-                root = ./.;
-                include = [
-                  "Cargo.toml"
-                  "Cargo.lock"
-                  "build.rs"
-                  "src"
-                ];
-              };
-              cargoLock.lockFile = ./Cargo.lock;
-              nativeBuildInputs = [ pkg-config ];
-              buildInputs = [ openssl ];
-              doCheck = false;
-            }
-          ) { };
+          ros2nix = craneLib.buildPackage {
+            pname = "ros2nix";
+            version = "0.1.0";
+            src = filter {
+              root = ./.;
+              include = [
+                "Cargo.toml"
+                "Cargo.lock"
+                "build.rs"
+                "src"
+              ];
+            };
+            nativeBuildInputs = [ pkgs.pkg-config ];
+            buildInputs = [ pkgs.openssl ];
+            doCheck = false;
+          };
         };
         devShells = {
           ci = pkgs.mkShell {
